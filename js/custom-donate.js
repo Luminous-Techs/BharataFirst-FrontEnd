23a6ac1058144d4881a57984a4919c3ce4495dcc
Hardik Makwana <hardikmakwana1432@gmail.com>
2021-11-28 14:51:52 +0530

    Update donate.html
    - donate form fixed
    - home id added for navigation at home page

--- js/custom-donate.js
+++ js/custom-donate.js
@@ -32,30 +32,3 @@ function myVal(){
     document.getElementById('scheme-amount' + i).checked = false;
   }
 }
-
-// Mobile Number Plugin JS
-
-var input = document.querySelector("#phone");
-      window.intlTelInput(input, {
-        // allowDropdown: false,
-        // autoHideDialCode: false,
-        // autoPlaceholder: "off",
-        // dropdownContainer: document.body,
-        // excludeCountries: ["us"],
-        // formatOnDisplay: false,
-        geoIpLookup: function(callback) {
-          $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-            var countryCode = (resp && resp.country) ? resp.country : "";
-            callback(countryCode);
-          });
-        },
-        // hiddenInput: "full_number",
-        // initialCountry: "auto",
-        // localizedCountries: { 'de': 'Deutschland' },
-        // nationalMode: false,
-        // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
-        // placeholderNumberType: "MOBILE",
-        preferredCountries: ['in'],
-        // separateDialCode: true,
-        utilsScript: "intl-tel-input/build/js/utils.js",
-      });
